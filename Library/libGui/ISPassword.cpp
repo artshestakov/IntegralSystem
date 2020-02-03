8fa87edc9d9f7ba6230642895926594ebfc3cd0e
Shestakov Artem <art.shestakov@icloud.com>
2020-02-03 23:52:59 +0300

    Перенос некоторых классов из libCore в libGui.

--- Library/libGui/ISPassword.cpp
+++ Library/libGui/ISPassword.cpp
@@ -12,114 +12,62 @@ QString ISPassword::GeneratePassword()
 	QStringList StringList = UID.split(SYMBOL_MINUS);
 	if (StringList.count() == 5)
 	{
-		QString GeneratedPassword = StringList[4];
-		return GeneratedPassword;
+		return StringList[4];
 	}
-
 	return QString();
 }
 //-----------------------------------------------------------------------------
 int ISPassword::GetWidthPassword(const QString &Password)
 {
-	int WeightPassword = 0;
-	int PasswordLenght = 0;
-	int CountFigures = 0;
-	int AltSymbols = 0;
-	int CountUpper = 0;
+	int WeightPassword = 0, PasswordLenght = 0, CountFigures = 0, AltSymbols = 0, CountUpper = 0;
 
 	{ //Равно 5, если количесво символов в пароле больше 5, или равно количеству символов
 		int Count = Password.length();
-		if (Count >= 5)
-		{
-			PasswordLenght = 5;
-		}
-		else
-		{
-			PasswordLenght = Count;
-		}
+		Count >= 5 ? PasswordLenght = 5 : PasswordLenght = Count;
 	}
 
 	{ //Равно 3, если количетсво цифр в пароле больше 3, иначе - равно количеству цифр
 		int Count = GetCountFigures(Password);
-		if (Count > 3)
-		{
-			CountFigures = 3;
-		}
-		else
-		{
-			CountFigures = Count;
-		}
+		Count > 3 ? CountFigures = 3 : CountFigures = Count;
 	}
 
 	{ //Равно 3, если число символов в пароле, отличных от букв, цифр и знаков подчеркивания, больше 3, иначе - равно количеству таких символов
 		int Count = GetCountAltSymbols(Password);
-		if (Count > 3)
-		{
-			AltSymbols = 3;
-		}
-		else
-		{
-			AltSymbols = Count;
-		}
+		Count > 3 ? AltSymbols = 3 : AltSymbols = Count;
 	}
 
 	{ //Равно 3, если количетсво букв в верхнем регистре больше 3, или количеству заглавных букв в противном случае
 		int Count = GetCountUpperLiters(Password);
-		if (Count > 3)
-		{
-			CountUpper = 3;
-		}
-		else
-		{
-			CountUpper = Count;
-		}
+		Count > 3 ? CountUpper = 3 : CountUpper = Count;
 	}
 
-	WeightPassword = ((PasswordLenght * 10) - 20) + (CountFigures * 10) + (AltSymbols * 15) + (CountUpper * 10);
-	return WeightPassword;
+	return ((PasswordLenght * 10) - 20) + (CountFigures * 10) + (AltSymbols * 15) + (CountUpper * 10);
 }
 //-----------------------------------------------------------------------------
 int ISPassword::GetCountFigures(const QString &Password)
 {
 	int CountFigures = 0;
-
-	for (int i = 0; i < Password.length(); ++i)
+	for (const QChar &Symbol : Password)
 	{
-		QString Symbol = Password.at(i);
 		if (QString(FIGURES_STRING).contains(Symbol))
 		{
-			CountFigures++;
+			++CountFigures;
 		}
 	}
-
 	return CountFigures;
 }
 //-----------------------------------------------------------------------------
 int ISPassword::GetCountAltSymbols(const QString &Password)
 {
 	int AltSymbols = 0;
-
-	QRegExp RegExp(REG_EXP_PASSWORD_ALT_SYMBOL);
-	QValidator *Validator = new QRegExpValidator(RegExp);
-
-	for (int i = 0; i < Password.length(); ++i)
+	for (const QChar &Symbol : Password)
 	{
-		int x = 0;
-
-		QString Symbol = Password.at(i);
-		QValidator::State ValidatorState = Validator->validate(Symbol, x);
-		if (ValidatorState == QValidator::Acceptable)
+		int Position = 0;
+		if (QRegExpValidator(QRegExp(REG_EXP_PASSWORD_ALT_SYMBOL)).validate(QString(Symbol), Position) == QRegExpValidator::Acceptable)
 		{
-			AltSymbols++;
+			++AltSymbols;
 		}
 	}
-
-	if (Validator)
-	{
-		delete Validator;
-		Validator = nullptr;
-	}
-
 	return AltSymbols;
 }
 //-----------------------------------------------------------------------------
@@ -128,10 +76,10 @@ int ISPassword::GetCountUpperLiters(const QString &Password)
 	int UpperSymbols = 0;
 	for (const QChar &Symbol : Password)
 	{
-		int Pos = 0;
-		if (QRegExpValidator(QRegExp(REG_EXP_PASSWORD_UPPER)).validate(QString(Symbol), Pos) == QValidator::Acceptable)
+		int Position = 0;
+		if (QRegExpValidator(QRegExp(REG_EXP_PASSWORD_UPPER)).validate(QString(Symbol), Position) == QRegExpValidator::Acceptable)
 		{
-			UpperSymbols++;
+			++UpperSymbols;
 		}
 	}
 
