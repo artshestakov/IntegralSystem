44cf2846cecc068a053f0c5257b176c3a7f821a5
Shestakov Artem <art.shestakov@icloud.com>
2020-02-10 17:40:26 +0300

    Промежуточный коммит.

--- Projects/Carat/main.cpp
+++ Projects/Carat/main.cpp
@@ -18,14 +18,14 @@ int main(int argc, char *argv[])
 	ISApplicationRunning ApplicationRunning(CARAT_UID);
 	if (!ApplicationRunning.TryToRun()) //Если приложение уже запущено
 	{
-		//ISDebug::ShowString(LANG("AlreadyStarted"));
+		ISDebug::ShowString("Application already started");
 		ISSystem::SleepSeconds(3);
 		return EXIT_SUCCESS;
 	}
 
-	if (!CONFIG_STRING(CONST_CONFIG_CONNECTION_LOGIN).length() || !CONFIG_STRING(CONST_CONFIG_CONNECTION_PASSWORD).length())
+	if (CONFIG_STRING(CONST_CONFIG_CONNECTION_LOGIN).isEmpty() || CONFIG_STRING(CONST_CONFIG_CONNECTION_PASSWORD).isEmpty())
 	{
-		//ISDebug::ShowString(LANG("NotLoginOrPassword"));
+		ISDebug::ShowString("Not found login or password in config");
 		return EXIT_FAILURE;
 	}
 
@@ -37,7 +37,7 @@ int main(int argc, char *argv[])
 
 	if (!ISLicense::GetInstance().Initialize()) //Если лицензия не инициализировалась
 	{
-		//ISDebug::ShowWarningString(LANG("License.Failed").arg(ISLicense::GetInstance().GetErrorString()));
+		ISDebug::ShowWarningString("License failed");
 		ISCommandLine::Pause();
 		return EXIT_SUCCESS;
 	}
