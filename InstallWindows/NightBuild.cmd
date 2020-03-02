f3a16d1edbab8ef54f77245615f2cb09da3e00a8
Shestakov Artem <art.shestakov@icloud.com>
2020-03-02 18:29:22 +0300

    В скрипте ночной сборки добавлено использовании рабочей директории.

--- InstallWindows/NightBuild.cmd
+++ InstallWindows/NightBuild.cmd
@@ -4,6 +4,7 @@ IF EXIST %1 (RMDIR /S /Q %1)
 REM Клонируем репозиторий и переходим в него, а затем в папку InstallWindows
 git clone https://github.com/artshestakov/IntegralSystem.git %1
 cd %1\InstallWindows
+pushd %1\InstallWindows
 
 call _Build_IntegralSystem_Debug_Win32.cmd
 call _Build_IntegralSystem_Release_Win32.cmd
