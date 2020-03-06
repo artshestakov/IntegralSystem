5aae702a46bc7b9f4c14d44f9fe436d01fcd3572
Shestakov Artem <art.shestakov@icloud.com>
2020-03-06 19:57:05 +0300

    Решение задачи #43.

---
+++ InstallWindows/BuildMorning.cmd
@@ -0,0 +1,18 @@
+@ECHO off
+SetLocal
+
+REM Скрипт принимает один единственный параметр в виде
+REM пути к рабочей папке проекта вида C:\Github\IntegralSystem
+
+REM Переходим в папку по переданному пути и делаем её рабочей (Working dir)
+cd %1
+pushd %1
+
+REM Получаем свежусь версию исходного кода из репозитория
+git pull
+
+call %1\InstallWindows\vcvars32.bat
+
+REM Запуск сборки
+MSBuild Solution.sln /t:Build /p:Configuration=Debug /p:Platform=Win32
+MSBuild Solution.sln /t:Build /p:Configuration=Release /p:Platform=Win32
