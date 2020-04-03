3a98361b31bd5745cdd28993556beca5790260a6
Shestakov Artem <art.shestakov@icloud.com>
2020-04-03 15:49:16 +0300

    Добавлена автоматическая остановка логгера при его разрушении.

--- Logger/ASLogger.cpp
+++ Logger/ASLogger.cpp
@@ -3,14 +3,27 @@
 //-----------------------------------------------------------------------------
 ASLogger::ASLogger()
 	: ErrorString("No error."),
-	Running(false)
+	LastIndex(0),
+	IsRunning(false),
+	IsFinished(false)
 {
 
 }
 //-----------------------------------------------------------------------------
 ASLogger::~ASLogger()
 {
+	//Останавливаем логгер
+	Mutex.lock();
+	IsRunning = false;
+	Mutex.unlock();
 
+	//Ждём когда он остановится и закрываем файл
+	while (!IsFinished)
+	{
+		Sleep(50);
+	}
+
+	File.close();
 }
 //-----------------------------------------------------------------------------
 ASLogger& ASLogger::Instance()
@@ -24,23 +37,23 @@ std::string ASLogger::GetErrorString() const
 	return ErrorString;
 }
 //-----------------------------------------------------------------------------
-bool ASLogger::Initialize(const std::string &Prefix)
+bool ASLogger::Initialize(const std::string &prefix)
 {
-	char Buffer[MAX_PATH];
-	if (!(GetModuleFileNameA(NULL, Buffer, MAX_PATH) > 0)) //Не удалось получить путь к исполняемому файлу
+	char buffer[MAX_PATH];
+	if (!(GetModuleFileNameA(NULL, buffer, MAX_PATH) > 0)) //Не удалось получить путь к исполняемому файлу
 	{
 		ErrorString = "Error getting module file name.";
 		return false;
 	}
 
-	std::string Temp = std::string(Buffer);
-	size_t Pos = Temp.rfind('\\');
+	std::string temp = std::string(buffer);
+	size_t pos = temp.rfind('\\');
 
 	//Если префикс файла не указан - получаем его из имени исполняемого файла, иначе - используем тот что указан
-	FilePrefix = Prefix.empty() ? FilePrefix = Temp.substr(Pos + 1, Temp.size() - Pos - 5) : Prefix;
+	FilePrefix = prefix.empty() ? temp.substr(pos + 1, temp.size() - pos - 5) : prefix;
 
 	//Получаем путь к директории с логами
-	PathDirLogs = Temp.substr(0, Pos + 1) + "Logs\\";
+	PathDirLogs = temp.substr(0, pos + 1) + "Logs\\";
 
 	if (!CreateLogDirectory()) //Ошибка при создании директорий
 	{
@@ -48,31 +61,49 @@ bool ASLogger::Initialize(const std::string &Prefix)
 	}
 
 	File.open(PathFile.c_str(), std::ios::app);
-	if (!File.is_open())
+	if (!File.is_open()) //Не удалось открыть файл
 	{
 		ErrorString = "Error open file \"" + PathFile + "\": " + std::string(strerror(errno));
 		return false;
 	}
 
-	Running = true;
+	IsRunning = true;
 	std::thread(&ASLogger::Worker, this).detach();
 	return true;
 }
 //-----------------------------------------------------------------------------
-void ASLogger::Shutdown()
+void ASLogger::Log(MessageType type_message, const std::string &string_message, const char *source_name, int source_line)
 {
-	File.close();
-}
-//-----------------------------------------------------------------------------
-void ASLogger::Log(MessageType message_type, const std::string &message_string, const char *SourceName, int Line)
-{
-	SYSTEMTIME ST;
-	GetSystemTime(&ST);
+	std::string string_complete;
+	if (type_message == MessageType::MT_Lite) //Если добавляется упрощенное сообщение (без типа, метки времени и т.д.)
+	{
+		string_complete = string_message;
+	}
+	else //Стандартное сообщение
+	{
+		SYSTEMTIME st;
+		GetSystemTime(&st);
 
-	char Buffer[MAX_PATH];
-	//sprintf(Buffer, "%02d.%02d.%d %02d:%02d:%02d.%03d %d", );
+		char buffer[MAX_PATH];
+		sprintf(buffer, "%02d.%02d.%d %02d:%02d:%02d.%03d %d", st.wDay, st.wMonth, st.wYear, st.wHour, st.wMinute, st.wSecond, st.wMilliseconds, GetCurrentThreadId());
 
-	std::string CompleteMessage;
+		std::stringstream string_stream;
+		string_stream << buffer << " [";
+		switch (type_message)
+		{
+		case MessageType::MT_Debug: string_stream << "Debug"; break;
+		case MessageType::MT_Info: string_stream << "Debug"; break;
+		case MessageType::MT_Warning: string_stream << "Debug"; break;
+		case MessageType::MT_Error: string_stream << "Debug"; break;
+		}
+		string_stream << "][" << source_name << ':' << source_line << "] " << string_message;
+		string_complete = string_stream.str();
+	}
+
+	Mutex.lock();
+	Array[LastIndex] = string_complete;
+	++LastIndex;
+	Mutex.unlock();
 }
 //-----------------------------------------------------------------------------
 bool ASLogger::CreateLogDirectory()
@@ -116,16 +147,23 @@ bool ASLogger::CreateLogDirectory()
 	//Формируем путь к текущему лог-файлу
 	char Buffer[MAX_PATH];
 	sprintf(Buffer, "%s%s_%02d.%02d.%02d.log", PathDirCurrent.c_str(), FilePrefix.c_str(), ST.wDay, ST.wMonth, ST.wYear);
-	PathFile = std::string(Buffer);
+	PathFile = Buffer;
 	return true;
 }
 //-----------------------------------------------------------------------------
 void ASLogger::Worker()
 {
-	while (Running)
+	while (IsRunning)
 	{
+		Sleep(50);
 
+		Mutex.lock();
+		if (LastIndex) //Если в очереди есть сообщения
+		{
+
+		}
 	}
+	IsFinished = true;
 }
 //-----------------------------------------------------------------------------
 bool ASLogger::CheckExistDir(const std::string &PathDir)
--- Logger/ASLogger.h
+++ Logger/ASLogger.h
@@ -5,6 +5,7 @@
 #include <string>
 #include <mutex>
 #include <fstream>
+#include <sstream>
 #include <array>
 #include <thread>
 //-----------------------------------------------------------------------------
@@ -15,6 +16,7 @@ class ASLogger
 public:
 	enum MessageType //Типы сообщений
 	{
+		MT_Lite, //Упрощенное
 		MT_Debug, //Отладка
 		MT_Info, //Информация
 		MT_Warning, //Предупреждение
@@ -25,10 +27,9 @@ public:
 	static ASLogger& Instance(); //Получить ссылку на объект логгера
 
 	std::string GetErrorString() const; //Получить описание ошибки
-	bool Initialize(const std::string &Prefix = std::string()); //Инициализировать логгер
-	void Shutdown(); //Остановка логгера
+	bool Initialize(const std::string &prefix = std::string()); //Инициализировать логгер
 
-	void Log(MessageType message_type, const std::string &message_string, const char *SourceName, int Line);
+	void Log(MessageType type_message, const std::string &string_message, const char *source_name, int source_line);
 
 private:
 	bool CreateLogDirectory(); //Создание директории
@@ -47,7 +48,9 @@ private:
 	std::string ErrorString; //Описание ошибки
 	std::mutex Mutex; //Мьютекс для массива
 	std::array<std::string, ARRAY_SIZE> Array; //Массив сообщений
-	bool Running; //Флаг работы логгера
+	size_t LastIndex;
+	bool IsRunning; //Флаг работы
+	bool IsFinished; //Флаг остановки
 
 	std::ofstream File; //Текущий лог-файл
 	std::string PathDirLogs; //Путь к корневой папке с логами
@@ -56,5 +59,11 @@ private:
 
 	std::string FilePrefix; //Префикс имени файла
 };
+//-----------------------------------------------------------------------------
+#define ASLOGGER_L(MESSAGE) ASLogger::Instance().Log(ASLogger::MT_Lite, MESSAGE, __FILE__, __LINE__)
+#define ASLOGGER_D(MESSAGE) ASLogger::Instance().Log(ASLogger::MT_Debug, MESSAGE, __FILE__, __LINE__)
+#define ASLOGGER_I(MESSAGE) ASLogger::Instance().Log(ASLogger::MT_Info, MESSAGE, __FILE__, __LINE__)
+#define ASLOGGER_W(MESSAGE) ASLogger::Instance().Log(ASLogger::MT_Warning, MESSAGE, __FILE__, __LINE__)
+#define ASLOGGER_E(MESSAGE) ASLogger::Instance().Log(ASLogger::MT_Error, MESSAGE, __FILE__, __LINE__)
 //-----------------------------------------------------------------------------
 #endif
