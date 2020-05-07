b2145fb6df561e87157d3464a16e9cbab7d60ddb
Shestakov Artem <art.shestakov@icloud.com>
2020-05-08 01:01:31 +0300

    Решение задачи #192.

--- Projects/CaratWorker/main.cpp
+++ Projects/CaratWorker/main.cpp
@@ -1,14 +1,41 @@
 #include "ISTcpServerWorker.h"
-#include "ISConstants.h"
+#include "ISCore.h"
+#include "ISLogger.h"
 //-----------------------------------------------------------------------------
 int main(int argc, char *argv[])
 {
 	QCoreApplication Application(argc, argv);
-	ISTcpServerWorker TcpServerWorker;
-	bool Result = TcpServerWorker.Run(CARAT_DEFAULT_PORT);
+	
+	QString ErrorString;
+	bool Result = ISCore::Startup(false, ErrorString);
 	if (!Result)
 	{
-		qDebug() << TcpServerWorker.GetErrorString();
+		ISLOGGER_E(ErrorString);
+		return EXIT_FAILURE;
+	}
+
+	Result = argc == 2;
+	if (Result) //Порт указан
+	{
+		int Port = std::atoi(argv[1]);
+		Result = Port > 0;
+		if (Result) //Порт валидный
+		{
+			ISTcpServerWorker TcpServerWorker;
+			Result = TcpServerWorker.Run(CARAT_DEFAULT_PORT);
+			if (!Result) //Не удалось запустить сервер
+			{
+				ISLOGGER_E(TcpServerWorker.GetErrorString());
+			}
+		}
+		else //Порт не валидный
+		{
+			ISLOGGER_E(QString("Invalid port %1").arg(Port));
+		}
+	}
+	else //Порт не указан
+	{
+		ISLOGGER_E("Port not specified");
 	}
 	return Result ? Application.exec() : EXIT_FAILURE;
 }
