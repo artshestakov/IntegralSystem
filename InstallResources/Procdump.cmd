c3427638a5c13eedf6874d2fc625588e56325bbf
artshestakov <art.shestakov@icloud.com>
2020-05-11 11:06:11 +0300

    Исключены лишние скрипты.

--- InstallResources/Procdump.cmd
+++
@@ -1,9 +0,0 @@
-SET FileDate=%DATE%
-
-SET FileTime=%TIME:~,8%
-SET FileTime=%FileTime::=.%
-
-SET FileName=IntegralSystem_%Filedate%_%FileTime%.dmp
-
-start %cd%\..\..\IntegralSystem.exe
-Procdump.exe -t IntegralSystem.exe %cd%\..\..\ProcDump\%FileName%
