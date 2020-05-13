614cde460abfdacd0e32e29c660f22ac28286430
Shestakov Artem <art.shestakov@icloud.com>
2020-05-13 11:10:05 +0300

    Правки тестового клиента.

--- Projects/ClientApplication/main.cpp
+++ Projects/ClientApplication/main.cpp
@@ -7,12 +7,13 @@ int main(int argc, char *argv[])
 {
 	QCoreApplication Application(argc, argv);
 
+	QString Host = argv[1];
 	quint16 Port = CARAT_DEFAULT_PORT;
-	if (ISTcpConnector::Instance().Connect("127.0.0.1", Port))
+	if (ISTcpConnector::Instance().Connect(Host, Port))
 	{
 		ISTcpQuery qAuth(API_AUTH);
-		qAuth.BindValue("Login", "shestakov");
-		qAuth.BindValue("Password", "454Trendy");
+		qAuth.BindValue("Login", "postgres");
+		qAuth.BindValue("Password", "adm777");
 		if (qAuth.Execute())
 		{
 			Port = qAuth.GetAnswer()["Port"].toInt();
@@ -21,22 +22,20 @@ int main(int argc, char *argv[])
 		{
 			qDebug() << qAuth.GetErrorString();
 		}
-		ISTcpConnector::Instance().Disconnect();
+
+		if (ISTcpConnector::Instance().Reconnect(Host, Port))
+		{
+			ISTcpQuery qTestQuery(API_TEST_QUERY);
+			if (qTestQuery.Execute())
+			{
+				qDebug() << qTestQuery.GetAnswer()["DateTime"].toString();
+			}
+			ISTcpConnector::Instance().Disconnect();
+		}
 	}
 	else
 	{
 		qDebug() << ISTcpConnector::Instance().GetErrorString();
 	}
-
-	if (ISTcpConnector::Instance().Connect("127.0.0.1", Port))
-	{
-		ISTcpQuery qTestQuery(API_TEST_QUERY);
-		if (qTestQuery.Execute())
-		{
-			qDebug() << qTestQuery.GetAnswer()["DateTime"].toString();
-		}
-	}
-
-	return Application.exec();
 }
 //-----------------------------------------------------------------------------
