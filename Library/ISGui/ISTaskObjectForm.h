5a3c9eed58a2609214636ace621defede95ded8f
Shestakov Artem <art.shestakov@icloud.com>
2020-07-28 12:59:33 +0300

    Решение задачи #368.

--- Library/ISGui/ISTaskObjectForm.h
+++ Library/ISGui/ISTaskObjectForm.h
@@ -10,5 +10,7 @@ class ISTaskObjectForm : public ISObjectFormBase
 public:
 	Q_INVOKABLE ISTaskObjectForm(ISNamespace::ObjectFormType form_type, PMetaTable *meta_table, QWidget *parent, int object_id = 0);
 	virtual ~ISTaskObjectForm();
+
+	bool Save() override;
 };
 //-----------------------------------------------------------------------------
