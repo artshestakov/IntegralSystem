9f6ee4bc14a09e0a8ab6b0faf50b50da3fc9f1f0
Shestakov Artem <art.shestakov@icloud.com>
2020-08-19 22:04:04 +0300

    Решение задачи #506.

--- Library/ISGui/ISDiscussionListForm.cpp
+++ Library/ISGui/ISDiscussionListForm.cpp
@@ -1,5 +1,16 @@
 #include "ISDiscussionListForm.h"
-#include "ISDiscussionObjectForm.h"
+#include "ISQuery.h"
+#include "ISInputDialog.h"
+#include "ISLocalization.h"
+#include "ISMessageBox.h"
+//-----------------------------------------------------------------------------
+static QString QI_DISCUSSION = PREPARE_QUERY("INSERT INTO _discussion(dson_tablename, dson_objectid, dson_message) "
+											  "VALUES(:TableName, :ObjectID, :Message) "
+											  "RETURNING dson_id");
+//-----------------------------------------------------------------------------
+static QString QU_DISCUSSION = PREPARE_QUERY("UPDATE _discussion SET "
+											 "dson_message = :Message "
+											 "WHERE dson_id = :DiscussionID");
 //-----------------------------------------------------------------------------
 ISDiscussionListForm::ISDiscussionListForm(QWidget *parent) : ISListBaseForm("_Discussion", parent)
 {
@@ -25,21 +36,43 @@ void ISDiscussionListForm::LoadData()
 //-----------------------------------------------------------------------------
 void ISDiscussionListForm::Create()
 {
-	if (ISDiscussionObjectForm(GetParentTableName(), GetParentObjectID(), QString()).Exec())
+	bool Ok = true;
+	QString Message = ISInputDialog::GetText(LANG("Discussion"), LANG("WhatDoYouWantToSay"), QVariant(), Ok);
+	if (Ok && !Message.isEmpty())
 	{
-		Update();
+		ISQuery qInsert(QI_DISCUSSION);
+		qInsert.BindValue(":TableName", GetParentTableName());
+		qInsert.BindValue(":ObjectID", GetParentObjectID());
+		qInsert.BindValue(":Message", Message);
+		if (qInsert.ExecuteFirst())
+		{
+			SetSelectObjectAfterUpdate(qInsert.ReadColumn("dson_id").toInt());
+			Update();
+		}
+		else
+		{
+			ISMessageBox::ShowCritical(this, qInsert.GetErrorString());
+		}
 	}
 }
 //-----------------------------------------------------------------------------
 void ISDiscussionListForm::CreateCopy()
 {
-	QString Message = GetCurrentRecordValue("Message").toString();
-
-	ISDiscussionObjectForm DiscussionObjectForm(GetParentTableName(), GetParentObjectID(), QString());
-	DiscussionObjectForm.SetEditMessageText(Message);
-	if (DiscussionObjectForm.Exec())
+	if (ISMessageBox::ShowQuestion(this, LANG("Message.Question.CopyDiscussion")))
 	{
-		Update();
+		ISQuery qInsert(QI_DISCUSSION);
+		qInsert.BindValue(":TableName", GetParentTableName());
+		qInsert.BindValue(":ObjectID", GetParentObjectID());
+		qInsert.BindValue(":Message", GetCurrentRecordValue("Message"));
+		if (qInsert.ExecuteFirst())
+		{
+			SetSelectObjectAfterUpdate(qInsert.ReadColumn("dson_id").toInt());
+			Update();
+		}
+		else
+		{
+			ISMessageBox::ShowCritical(this, qInsert.GetErrorString());
+		}
 	}
 }
 //-----------------------------------------------------------------------------
@@ -48,11 +81,22 @@ void ISDiscussionListForm::Edit()
 	QString Message = GetCurrentRecordValue("Message").toString();
 	int DiscussionID = GetCurrentRecordValue("ID").toInt();
 
-	ISDiscussionObjectForm DiscussionObjectForm(GetParentTableName(), GetParentObjectID(), Message);
-	DiscussionObjectForm.SetUID(DiscussionID);
-	if (DiscussionObjectForm.Exec())
+	bool Ok = true;
+	QString NewMessage = ISInputDialog::GetText(LANG("Discussion"), LANG("WhatDoYouWantToSay"), Message, Ok);
+	if (Ok && !NewMessage.isEmpty() && NewMessage != Message)
 	{
-		Update();
+		ISQuery qUpdate(QU_DISCUSSION);
+		qUpdate.BindValue(":Message", NewMessage);
+		qUpdate.BindValue(":DiscussionID", DiscussionID);
+		if (qUpdate.Execute())
+		{
+			SetSelectObjectAfterUpdate(DiscussionID);
+			Update();
+		}
+		else
+		{
+			ISMessageBox::ShowCritical(this, qUpdate.GetErrorString());
+		}
 	}
 }
 //-----------------------------------------------------------------------------
