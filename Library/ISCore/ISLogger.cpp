a2b641627de60b8544dac670cc245f8e87f989a7
Shestakov Artem <art.shestakov@icloud.com>
2020-10-27 00:49:35 +0300

    Решение задачи #932.

--- Library/ISCore/ISLogger.cpp
+++ Library/ISCore/ISLogger.cpp
@@ -69,22 +69,32 @@ void ISLogger::Shutdown()
 void ISLogger::Log(MessageType message_type, const QString &component, const QString &string)
 {
 	QString string_complete = QDateTime::currentDateTime().toString(FORMAT_DATE_TIME_V9) + '\t' + QString::number(GET_CURRENT_THREAD_ID()) + '\t';
-	switch (message_type)
+	if (message_type == MT_Null || message_type == MT_Lite) //ƒл€ сообщени€ типа MT_Null и MT_Lite заголовок не добавл€ем
 	{
-	case MessageType::MT_Null: string_complete.clear(); break;
-	case MessageType::MT_Lite: string_complete.clear(); break;
-	case MessageType::MT_Debug: string_complete += "[Debug]"; break;
-	case MessageType::MT_Info: string_complete += "[Info]"; break;
-	case MessageType::MT_Warning: string_complete += "[Warning]"; break;
-	case MessageType::MT_Error: string_complete += "[Error]"; break;
-	case MessageType::MT_Trace: string_complete += "[Trace]"; break;
-	case MessageType::MT_Assert: string_complete += "[Assert]"; break;
+		string_complete = string;
 	}
-	if (!component.isEmpty())
+	else
 	{
-		string_complete += '[' + component + ']';
+		string_complete = QDateTime::currentDateTime().toString(FORMAT_DATE_TIME_V9) + '\t' + QString::number(GET_CURRENT_THREAD_ID()) + '\t';
+		switch (message_type)
+		{
+		case MessageType::MT_Null: string_complete.clear(); break;
+		case MessageType::MT_Lite: string_complete.clear(); break;
+		case MessageType::MT_Debug: string_complete += "[Debug]"; break;
+		case MessageType::MT_Info: string_complete += "[Info]"; break;
+		case MessageType::MT_Warning: string_complete += "[Warning]"; break;
+		case MessageType::MT_Error: string_complete += "[Error]"; break;
+		case MessageType::MT_Trace: string_complete += "[Trace]"; break;
+		case MessageType::MT_Assert: string_complete += "[Assert]"; break;
+		}
+
+		if (!component.isEmpty())
+		{
+			string_complete += '[' + component + ']';
+		}
+
+		string_complete += ' ' + string;
 	}
-	string_complete += ' ' + string;
 
 #ifdef DEBUG //¬ отладочной версии выводим строку в консоль
 	OutputToConsole(string_complete);
