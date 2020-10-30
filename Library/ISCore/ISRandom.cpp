8e959d675b9b89283a79acf103943954bf335d9f
artshestakov <art.shestakov@icloud.com>
2020-10-30 17:29:06 +0300

    Убрано предупреждение из модуля ISRandom.

--- Library/ISCore/ISRandom.cpp
+++ Library/ISCore/ISRandom.cpp
@@ -25,9 +25,7 @@ qint64 ISRandom::Get(qint64 Minimum, qint64 Maximum)
 qint64 ISRandom::Get()
 {
 	Digit ^= (Digit << 21);
-#pragma warning (disable: 4293)
 	Digit ^= (Digit >> 35);
-#pragma warning (default: 4293)
 	Digit ^= (Digit << 4);
 	return Digit;
 }
