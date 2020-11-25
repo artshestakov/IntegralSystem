45889c8edfe3591d71257912b95a7d76272ca38b
Shestakov Artem <art.shestakov@icloud.com>
2020-11-25 14:36:39 +0300

    Решение задачи #1036.

--- Library/ISCore/ISConstants.h
+++ Library/ISCore/ISConstants.h
@@ -46,7 +46,7 @@ const char LOCALIZATION_FILE_CARAT[] = "Carat";
 //REG_EXP----------------------------------------------------------------------
 const char REG_EXP_IP_ADDRESS[] = "([0-9]{1,3}\\.){3}[0-9]{1,3}(\\/([0-9]|[1-2][0-9]|3[0-2]))?"; //IP-адресс
 const char REG_EXP_UID[] = "UID=\"\\{\\w{8}-\\w{4}-\\w{4}-\\w{4}-\\w{12}\\}\""; //Уникальный идентфикатор из мета-данных
-const char REG_EXP_LOGIN[] = "^([0-9A-Za-z]+)$";
+const char REG_EXP_LOGIN[] = "^([0-9A-Za-z._]+)$";
 const char REG_EXP_PASSWORD_ALT_SYMBOL[] = "^[^а-яА-ЯёЁa-zA-Z0-9_]+$";
 const char REG_EXP_PASSWORD_UPPER[] = "^[А-ЯA-Z]+$";
 const char REG_EXP_URL[] = "^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$"; //Ссылка на сайт или страницу в интернете
