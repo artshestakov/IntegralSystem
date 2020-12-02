b8aeb30a6d9086fb2ab9ccbf22625f1fcf68233f
Shestakov Artem <art.shestakov@icloud.com>
2020-12-02 23:42:40 +0300

    Удаление устаревшего скрипта.

--- Scripts/CurrentLog.sh
+++
@@ -1,4 +0,0 @@
-CURRENT_DAY=$(date +"%d")
-CURRENT_MONTH=$(date +"%m")
-CURRENT_YEAR=$(date +"%Y")
-watch -n1 cat /opt/IntegralSystem/Logs/$CURRENT_YEAR/$CURRENT_MONTH/Carat_$CURRENT_DAY.$CURRENT_MONTH.$CURRENT_YEAR.log
