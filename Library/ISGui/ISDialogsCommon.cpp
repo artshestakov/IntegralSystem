bb34a5c28b2dcc9a6df4ccfa88e278ad5e141220
Shestakov Artem <art.shestakov@icloud.com>
2021-01-21 12:13:06 +0300

    Незначительные исправления.

--- Library/ISGui/ISDialogsCommon.cpp
+++ Library/ISGui/ISDialogsCommon.cpp
@@ -18,23 +18,23 @@ ISFileDialog::~ISFileDialog()
 //-----------------------------------------------------------------------------
 QString ISFileDialog::GetOpenFileNameImage(QWidget *parent)
 {
-	QString FilterString;
-	FilterString += LANG("File.Filter.Images") + ";;";
-	FilterString += LANG("File.Filter.Image.PNG") + ";;";
-	FilterString += LANG("File.Filter.Image.JPEG") + ";;";
-	FilterString += LANG("File.Filter.Image.JPG") + ";;";
-	FilterString += LANG("File.Filter.Image.DDS") + ";;";
-	FilterString += LANG("File.Filter.Image.GIF") + ";;";
-	FilterString += LANG("File.Filter.Image.ICNS") + ";;";
-	FilterString += LANG("File.Filter.Image.ICO") + ";;";
-	FilterString += LANG("File.Filter.Image.SVG") + ";;";
-	FilterString += LANG("File.Filter.Image.TGA") + ";;";
-	FilterString += LANG("File.Filter.Image.TIFF") + ";;";
-	FilterString += LANG("File.Filter.Image.WBMP") + ";;";
-	FilterString += LANG("File.Filter.Image.BMP") + ";;";
-	FilterString += LANG("File.Filter.Image.WEBP");
+	QStringList FilterList = QStringList()
+		<< LANG("File.Filter.Images")
+		<< LANG("File.Filter.Image.PNG")
+		<< LANG("File.Filter.Image.JPEG")
+		<< LANG("File.Filter.Image.JPG")
+		<< LANG("File.Filter.Image.DDS")
+		<< LANG("File.Filter.Image.GIF")
+		<< LANG("File.Filter.Image.ICNS")
+		<< LANG("File.Filter.Image.ICO")
+		<< LANG("File.Filter.Image.SVG")
+		<< LANG("File.Filter.Image.TGA")
+		<< LANG("File.Filter.Image.TIFF")
+		<< LANG("File.Filter.Image.WBMP")
+		<< LANG("File.Filter.Image.BMP")
+		<< LANG("File.Filter.Image.WEBP");
 
-	QString Path = ISFileDialog::getOpenFileName(parent, LANG("FileDialog.Image.Select.Title"), PROPERTY_GET("PathLastDirectory").toString(), FilterString);
+	QString Path = ISFileDialog::getOpenFileName(parent, LANG("FileDialog.Image.Select.Title"), PROPERTY_GET("PathLastDirectory").toString(), FilterList.join(";;"));
 	if (!Path.isEmpty())
 	{
 		PROPERTY_SET("PathLastDirectory", QFileInfo(Path).dir().path());
@@ -44,23 +44,23 @@ QString ISFileDialog::GetOpenFileNameImage(QWidget *parent)
 //-----------------------------------------------------------------------------
 QString ISFileDialog::GetSaveFileNameImage(QWidget *parent, const QString &FileName)
 {
-	QString FilterString;
-	FilterString += LANG("File.Filter.Images") + ";;";
-	FilterString += LANG("File.Filter.Image.PNG") + ";;";
-	FilterString += LANG("File.Filter.Image.JPEG") + ";;";
-	FilterString += LANG("File.Filter.Image.JPG") + ";;";
-	FilterString += LANG("File.Filter.Image.DDS") + ";;";
-	FilterString += LANG("File.Filter.Image.GIF") + ";;";
-	FilterString += LANG("File.Filter.Image.ICNS") + ";;";
-	FilterString += LANG("File.Filter.Image.ICO") + ";;";
-	FilterString += LANG("File.Filter.Image.SVG") + ";;";
-	FilterString += LANG("File.Filter.Image.TGA") + ";;";
-	FilterString += LANG("File.Filter.Image.TIFF") + ";;";
-	FilterString += LANG("File.Filter.Image.WBMP") + ";;";
-	FilterString += LANG("File.Filter.Image.BMP") + ";;";
-	FilterString += LANG("File.Filter.Image.WEBP");
+	QStringList FilterList = QStringList()
+		<< LANG("File.Filter.Images")
+		<< LANG("File.Filter.Image.PNG")
+		<< LANG("File.Filter.Image.JPEG")
+		<< LANG("File.Filter.Image.JPG")
+		<< LANG("File.Filter.Image.DDS")
+		<< LANG("File.Filter.Image.GIF")
+		<< LANG("File.Filter.Image.ICNS")
+		<< LANG("File.Filter.Image.ICO")
+		<< LANG("File.Filter.Image.SVG")
+		<< LANG("File.Filter.Image.TGA")
+		<< LANG("File.Filter.Image.TIFF")
+		<< LANG("File.Filter.Image.WBMP")
+		<< LANG("File.Filter.Image.BMP")
+		<< LANG("File.Filter.Image.WEBP");
 
-	QString Path = ISFileDialog::getSaveFileName(parent, LANG("FileDialog.Image.Save.Title"), PROPERTY_GET("PathLastDirectory").toString() + (FileName.isEmpty() ? QString() : '/' + FileName), FilterString);
+	QString Path = ISFileDialog::getSaveFileName(parent, LANG("FileDialog.Image.Save.Title"), PROPERTY_GET("PathLastDirectory").toString() + (FileName.isEmpty() ? QString() : '/' + FileName), FilterList.join(";;"));
 	if (!Path.isEmpty())
 	{
 		PROPERTY_SET("PathLastDirectory", QFileInfo(Path).dir().path());
