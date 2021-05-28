67db259afc1b81975c2283f59d1986f783de1a18
Shestakov Artem <art.shestakov@icloud.com>
2021-05-28 22:25:28 +0300

    Модернизирован модуль ассерта.

--- LibrarySTD/ISCore/ISAssert.cpp
+++ LibrarySTD/ISCore/ISAssert.cpp
@@ -1,46 +1,49 @@
 #include "ISAssert.h"
 #include "ISLogger.h"
 //-----------------------------------------------------------------------------
-void ISASSERT::Assert(bool Boolean, const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName)
+void ISASSERT::Assert(bool Boolean, const std::string &FalseMessage, const char *FileName, int Line)
 {
     if (!Boolean)
     {
-        ShowAssert(FalseMessage, FunctionName, Line, FileName);
+        ShowAssert(FalseMessage, FileName, Line);
     }
 }
 //-----------------------------------------------------------------------------
-void ISASSERT::Assert(int Integer, const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName)
+void ISASSERT::Assert(int Integer, const std::string &FalseMessage, const char *FileName, int Line)
 {
     if (!Integer)
     {
-        ShowAssert(FalseMessage, FunctionName, Line, FileName);
+        ShowAssert(FalseMessage, FileName, Line);
     }
 }
 //-----------------------------------------------------------------------------
-void ISASSERT::Assert(const std::string &String, const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName)
+void ISASSERT::Assert(const std::string &String, const std::string &FalseMessage, const char *FileName, int Line)
 {
     if (String.empty())
     {
-        ShowAssert(FalseMessage, FunctionName, Line, FileName);
+        ShowAssert(FalseMessage, FileName, Line);
     }
 }
 //-----------------------------------------------------------------------------
-void ISASSERT::Assert(size_t SizeT, const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName)
+void ISASSERT::Assert(size_t SizeT, const std::string &FalseMessage, const char *FileName, int Line)
 {
     if (!SizeT)
     {
-        ShowAssert(FalseMessage, FunctionName, Line, FileName);
+        ShowAssert(FalseMessage, FileName, Line);
     }
 }
 //-----------------------------------------------------------------------------
-void ISASSERT::ShowAssert(const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName)
+void ISASSERT::ShowAssert(const std::string &FalseMessage, const char *FileName, int Line)
 {
-    ISLOGGER_A(FalseMessage.c_str());
-    std::stringstream StringStream;
-    StringStream << "Assert: " << FalseMessage << std::endl;
-    StringStream << "Source file: " << FileName << std::endl;
-    StringStream << "Line: " << Line << std::endl;
-    StringStream << "Function: " << FunctionName;
-    throw std::runtime_error(StringStream.str());
+    //Формируем строку с описание ассерта
+    std::string String = ISAlgorithm::StringF("%s\nSource file: %s Line: %d",
+        FalseMessage.c_str(), FileName, Line);
+
+    //Логируем и немного ждём
+    ISLOGGER_A(String.c_str());
+    ISSleep(500);
+
+    //Вызываем исключение
+    throw std::runtime_error(String);
 }
 //-----------------------------------------------------------------------------
--- LibrarySTD/ISCore/ISAssert.h
+++ LibrarySTD/ISCore/ISAssert.h
@@ -5,22 +5,18 @@
 #include "StdAfx.h"
 #include "ISAlgorithm.h"
 //-----------------------------------------------------------------------------
-#ifdef WIN32
-#define IS_ASSERT(CONDITION, FALSE_MESSAGE) ISASSERT::Assert(CONDITION, FALSE_MESSAGE, __FUNCTION__, __LINE__, __FILE__)
-#else
-#define IS_ASSERT(CONDITION, FALSE_MESSAGE) ISASSERT::Assert(CONDITION, FALSE_MESSAGE, __PRETTY_FUNCTION__, __LINE__, __FILE__)
-#endif
+#define IS_ASSERT(CONDITION, FALSE_MESSAGE) ISASSERT::Assert(CONDITION, FALSE_MESSAGE, __FILE__, __LINE__)
 //-----------------------------------------------------------------------------
 class ISASSERT
 {
 public:
-    static void Assert(bool Boolean, const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName);
-    static void Assert(int Integer, const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName);
-    static void Assert(const std::string &String, const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName);
-    static void Assert(size_t SizeT, const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName);
+    static void Assert(bool Boolean, const std::string &FalseMessage, const char *FileName, int Line);
+    static void Assert(int Integer, const std::string &FalseMessage, const char *FileName, int Line);
+    static void Assert(const std::string &String, const std::string &FalseMessage, const char *FileName, int Line);
+    static void Assert(size_t SizeT, const std::string &FalseMessage, const char *FileName, int Line);
 
 protected:
-    static void ShowAssert(const std::string &FalseMessage, const std::string &FunctionName, int Line, const char *FileName);
+    static void ShowAssert(const std::string &FalseMessage, const char *FileName, int Line);
 };
 //-----------------------------------------------------------------------------
 #endif
