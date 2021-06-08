c39d60cfff3a0e3db272dcbe6d3eb7950638917d
Shestakov Artem <artem.shestakov@fleetcor.ru>
2021-06-08 19:38:31 +0300

    IS-2077: Продумать дефайны из ISGui.h.

--- Projects/IntegralSystem/ISGui.h
+++ Projects/IntegralSystem/ISGui.h
@@ -11,15 +11,6 @@
 #include "ISAssert.h"
 #include "ISTypedefs.h"
 //-----------------------------------------------------------------------------
-
-//ќбработка дробного значени€
-#define DOUBLE_PREPAREN(d, n) QString::number(d, 'f', n)
-#define DOUBLE_PREPARE(d) DOUBLE_PREPAREN(d, SETTING_DATABASE_VALUE_INT(CONST_UID_DATABASE_SETTING_OTHER_NUMBERSIMBOLSAFTERCOMMA))
-
-//ќбработка денежного значени€ (кол-во знаков после зап€той должно быть 2)
-#define DOUBLE_PREPAREM(d) QString::number(d, 'f', 2)
-
-//-----------------------------------------------------------------------------
 class ISGui
 {
 public:
