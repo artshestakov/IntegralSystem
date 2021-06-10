f7cdbe85aa2410f6fe3dd5e34522b7b6f397928c
Shestakov Artem <artem.shestakov@fleetcor.ru>
2021-06-10 16:45:36 +0300

    IS-2112: Поддержка параметров со значениями.

--- Library/ISServer/ISArguments.cpp
+++ Library/ISServer/ISArguments.cpp
@@ -2,10 +2,8 @@
 #include "ISConstants.h"
 #include "ISAlgorithm.h"
 //-----------------------------------------------------------------------------
-ISArguments::ISArguments(int argc, char **argv)
-    : ErrorString(STRING_NO_ERROR),
-    ARGC(argc),
-    ARGV(argv)
+ISArguments::ISArguments()
+    : ErrorString(STRING_NO_ERROR)
 {
 
 }
@@ -25,8 +23,13 @@ std::string ISArguments::GetHelp() const
     std::string Result = "Arguments:\n";
     for (const ISArgumentItem &Item : Vector)
     {
-        Result.append(ISAlgorithm::StringF("  %s,\t%-20s%-20s%\n",
-            Item.SmallName.c_str(), Item.FullName.c_str(), Item.Description.c_str()));
+        std::string FullName = Item.FullName;
+        if (Item.Type == ArgumentType::Parameter)
+        {
+            FullName.append("=[VALUE]");
+        }
+        Result.append(ISAlgorithm::StringF("  %s,\t%-25s%-20s%\n",
+            Item.SmallName.c_str(), FullName.c_str(), Item.Description.c_str()));
     }
     ISAlgorithm::StringChop(Result, 1);
     return Result;
@@ -34,21 +37,73 @@ std::string ISArguments::GetHelp() const
 //-----------------------------------------------------------------------------
 void ISArguments::AddFlag(const std::string &SmallName, const std::string &FullName, const std::string &Description)
 {
-    ISArgumentItem ArgumentItem;
-    ArgumentItem.Type = ArgumentType::Flag;
-    ArgumentItem.SmallName = SmallName;
-    ArgumentItem.FullName = FullName;
-    ArgumentItem.Description = Description;
-    Vector.emplace_back(ArgumentItem);
+    Add(ArgumentType::Flag, SmallName, FullName, Description);
 }
 //-----------------------------------------------------------------------------
-bool ISArguments::IsExist(const char *Name)
+void ISArguments::AddParameter(const std::string &SmallName, const std::string &FullName, const std::string &Description)
 {
-    for (int i = 1; i < ARGC; ++i)
+    Add(ArgumentType::Parameter, SmallName, FullName, Description);
+}
+//-----------------------------------------------------------------------------
+bool ISArguments::Parse(int argc, char **argv)
+{
+    size_t Pos = 0;
+    for (int i = 1; i < argc; ++i)
     {
-        if (strcmp(ARGV[i], Name) == 0)
+        //Проверяем, есть ли символ '=' в текущем аргументе
+        std::string Argument(argv[i]), Value;
+        Pos = Argument.find('=');
+        if (Pos != NPOS) //Символ есть - считаем что значение есть
         {
-            return true;
+            Value = Argument.substr(Pos + 1, Argument.size() - Pos - 1);
+            ISAlgorithm::StringChop(Argument, Argument.size() - Pos);
+
+            //Если значение пустое - ошибка
+            if (Value.empty())
+            {
+                ErrorString = ISAlgorithm::StringF("Empty value parameter \"%s\"", Argument.c_str());
+                return false;
+            }
+        }
+
+        //Ищем такой флаг/параметр в векторе
+        //Если его нет - ошибка
+        ArgumentType Type = ArgumentType::Unknown;
+        if (!ExistInVector(Argument, Type))
+        {
+            ErrorString = ISAlgorithm::StringF("Argument \"%s\" not support",
+                Argument.c_str());
+            return false;
+        }
+
+        //Запрещаем указание параметров для флагов
+        //Ситуация, когда усовный флаг -t имеет значение. Такого быть не должно
+        if (Pos != NPOS && Type == ArgumentType::Flag)
+        {
+            ErrorString = ISAlgorithm::StringF("Illegal value \"%s\" from flag \"%s\"",
+                Value.c_str(), Argument.c_str());
+            return false;
+        }
+
+        //Установим значение для параметра
+        if (Pos != NPOS)
+        {
+            SetValue(Argument, Value);
+        }
+
+        //Установим, что такой аргумент включён
+        SetEnabled(Argument);
+    }
+    return true;
+}
+//-----------------------------------------------------------------------------
+bool ISArguments::IsExist(const std::string &Name)
+{
+    for (ISArgumentItem &Item : Vector)
+    {
+        if (Name == Item.SmallName || Name == Item.FullName)
+        {
+            return Item.Enabled;
         }
     }
     return false;
@@ -57,5 +112,66 @@ bool ISArguments::IsExist(const char *Name)
 bool ISArguments::IsEmpty()
 {
     return ARGC == 1;
+}
+//-----------------------------------------------------------------------------
+std::string ISArguments::GetValue(const std::string &Name)
+{
+    std::string Value;
+    for (const ISArgumentItem &Item : Vector)
+    {
+        if (Item.SmallName == Name || Item.FullName == Name)
+        {
+            Value = Item.Value;
+            break;
+        }
+    }
+    return Value;
+}
+//-----------------------------------------------------------------------------
+void ISArguments::Add(ArgumentType Type, const std::string &SmallName, const std::string &FullName, const std::string &Description)
+{
+    ISArgumentItem ArgumentItem;
+    ArgumentItem.Type = Type;
+    ArgumentItem.SmallName = SmallName;
+    ArgumentItem.FullName = FullName;
+    ArgumentItem.Description = Description;
+    Vector.emplace_back(ArgumentItem);
+}
+//-----------------------------------------------------------------------------
+bool ISArguments::ExistInVector(const std::string &Name, ArgumentType &Type)
+{
+    for (const ISArgumentItem &Item : Vector)
+    {
+        if (Name == Item.SmallName || Name == Item.FullName)
+        {
+            Type = Item.Type;
+            return true;
+        }
+    }
+    return false;
+}
+//-----------------------------------------------------------------------------
+void ISArguments::SetValue(const std::string &Name, const std::string &Value)
+{
+    for (ISArgumentItem &Item : Vector)
+    {
+        if (Item.Type == ArgumentType::Parameter && (Name == Item.SmallName || Name == Item.FullName))
+        {
+            Item.Value = Value;
+            break;
+        }
+    }
+}
+//-----------------------------------------------------------------------------
+void ISArguments::SetEnabled(const std::string &Name)
+{
+    for (ISArgumentItem &Item : Vector)
+    {
+        if (Name == Item.SmallName || Name == Item.FullName)
+        {
+            Item.Enabled = true;
+            break;
+        }
+    }
 }
 //-----------------------------------------------------------------------------
--- Library/ISServer/ISArguments.h
+++ Library/ISServer/ISArguments.h
@@ -9,37 +9,48 @@ class ISArguments
 public:
     enum class ArgumentType //Тип аргументов командной строки
     {
-        Unknown,
-        Flag,
+        Unknown, //Неизвестно
+        Flag, //Флаг
+        Parameter, //Параметр
     };
 
 private:
     struct ISArgumentItem
     {
         ISArgumentItem()
-            : Type(ISArguments::ArgumentType::Unknown),
-            NotNull(false)
+            : Type(ArgumentType::Unknown),
+            Enabled(false)
         {
 
         }
 
-        ISArguments::ArgumentType Type;
-        bool NotNull;
-        std::string SmallName;
-        std::string FullName;
-        std::string Description;
+        ISArguments::ArgumentType Type; //Тип аргумента
+        bool Enabled;
+        std::string SmallName; //Упрощенное название
+        std::string FullName; //Полное название
+        std::string Description; //Описание
+        std::string Value; //Значение (заполняется только когда Type=ArgumentType::Parameter)
     };
 
 public:
-    ISArguments(int argc, char **argv);
+    ISArguments();
     ~ISArguments();
 
     const std::string& GetErrorString() const;
     std::string GetHelp() const;
     void AddFlag(const std::string &SmallName, const std::string &FullName, const std::string &Description);
+    void AddParameter(const std::string &SmallName, const std::string &FullName, const std::string &Description);
+    bool Parse(int argc, char **argv);
 
-    bool IsExist(const char *Name);
+    bool IsExist(const std::string &Name);
     bool IsEmpty();
+    std::string GetValue(const std::string &Name);
+
+private:
+    void Add(ArgumentType Type, const std::string &SmallName, const std::string &FullName, const std::string &Description);
+    bool ExistInVector(const std::string &Name, ArgumentType &Type);
+    void SetValue(const std::string &Name, const std::string &Value);
+    void SetEnabled(const std::string &Name);
 
 private:
     std::string ErrorString;
